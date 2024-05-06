9f5f536c566249b48e355bebaf193d1e74f278bb
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-05-06 15:42:52 +0900

    v0.0.5

--- src/template/utils/__init__.py
+++ src/template/utils/__init__.py
@@ -5,4 +5,4 @@ from ._utils import dummy_func, is_installed
 
 # _utils.pyだと、_が入っているのでドキュメント化されない。
 # ドキュメント化したい場合は、ぼジュールメソッドとして登録するため、__all__に入れる。
-__all__ = ["is_installed"]
+__all__ = ["is_installed", "dummy_func"]
